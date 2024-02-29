639a7d574f6ea44d9fa10ef70cb4251ef4f185c1
sgartner03 <simon.gartner@gepardec.com>
2024-02-29 12:46:59 +0100

    [Gepardec/mega#721] Deleted Null Checks and dead code

--- src/app/domains/monthly-report/feature-monthly-report/leads/leads.component.ts
+++ src/app/domains/monthly-report/feature-monthly-report/leads/leads.component.ts
@@ -1,9 +1,5 @@
-import {Component, Inject, Input, LOCALE_ID, OnChanges, OnDestroy, OnInit, SimpleChanges} from '@angular/core';
+import {Component, Input, OnChanges, SimpleChanges} from '@angular/core';
 import {MonthlyReport} from '@mega/monthly-report/data-model';
-import {MonthlyReportService} from '@mega/monthly-report/data-service';
-import {Subscription, zip} from 'rxjs';
-import {tap} from 'rxjs/operators';
-import {toMonthYearString} from '@mega/shared/util-common';
 import {MatTableModule} from '@angular/material/table';
 import {NgxSkeletonLoaderModule} from 'ngx-skeleton-loader';
 import {NgIf} from '@angular/common';
@@ -22,41 +18,22 @@ import {LeadsData} from '../../data-model/LeadsData';
     MatTableModule,
   ]
 })
-export class LeadsComponent implements OnInit, OnChanges, OnDestroy {
+export class LeadsComponent implements OnChanges {
 
   @Input() monthlyReport: MonthlyReport;
 
   displayedColumns = ['description', 'value'];
-  public selectedDateStr;
-  private dateSelectionSub: Subscription;
 
-  constructor(public monthlyReportService: MonthlyReportService, @Inject(LOCALE_ID) private locale: string) {
-  }
-
-  ngOnInit() {
-    this.dateSelectionSub = zip(this.monthlyReportService.selectedYear, this.monthlyReportService.selectedMonth)
-      .pipe(
-        tap(value => {
-          this.selectedDateStr = toMonthYearString(value[0], value[1] - 1, this.locale);
-        })
-      ).subscribe();
-  }
-
-  ngOnDestroy(): void {
-    this.dateSelectionSub?.unsubscribe();
-  }
 
   toLeadsData(): LeadsData[] {
     return [
       {
         description: 'Gilden-Lead',
-        value: this.monthlyReport.guildLead == null ?
-          'nicht vorhanden' : this.monthlyReport.guildLead
+        value: this.monthlyReport.guildLead
       },
       {
         description: 'Interner Projektleiter',
-        value: this.monthlyReport.internalProjectLead == null ?
-          'nicht vorhanden' : this.monthlyReport.internalProjectLead
+        value: this.monthlyReport.internalProjectLead
       },
     ];
   }
