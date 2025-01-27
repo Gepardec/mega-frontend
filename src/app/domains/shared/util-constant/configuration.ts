c89a3f481442ca8b49169968fa4e08e10699a359
Tod Oliver <olivertod11@yahoo.de>
2025-01-27 20:28:57 +0100

    [Gepardec/mega#763] Refactor date handling to use YearMonth consistently.
    Replaced LocalDate with YearMonth across services, tests, and utility functions for improved clarity and accuracy when working with month-specific data. Adjusted method signatures and logic to align with the new type, simplifying handling of start and end dates within months.

--- src/app/domains/shared/util-constant/configuration.ts
+++ src/app/domains/shared/util-constant/configuration.ts
@@ -1,18 +1,19 @@
 export const configuration = {
   PAGE_URLS: {
-    MONTHLY_REPORT: 'monthlyReport',
-    OFFICE_MANAGEMENT: 'officeManagement',
-    PROJECT_MANAGEMENT: 'projectManagement',
-    LOGIN: 'login',
-    ERROR: 'error'
+    MONTHLY_REPORT: "monthlyReport",
+    OFFICE_MANAGEMENT: "officeManagement",
+    PROJECT_MANAGEMENT: "projectManagement",
+    LOGIN: "login",
+    ERROR: "error",
   },
 
   OFFICE_MANAGEMENT_SEGMENT: `view/index.php?menu=MitarbeiterVerwaltungMgr&
   modelContentMenu=true&mgr=MitarbeiterProjektzeitMgr&contentModelId=`,
   PROJECT_MANAGEMENT_SEGMENT: `view/index.php?menu=ProjektVerwaltungMgr&modelContentMenu=true&contentModelId=`,
 
-  dateFormat: 'YYYY-MM-DD',
-  dateFormatNg: 'yyyy-MM-dd',
+  dateFormat: "YYYY-MM-DD",
+  yearMonthFormat: "YYYY-MM-DD",
+  dateFormatNg: "yyyy-MM-dd",
 
   LogLevel: {
     All: 0,
@@ -21,14 +22,14 @@ export const configuration = {
     Warn: 3,
     Error: 4,
     Fatal: 5,
-    Off: 6
+    Off: 6,
   },
 
   logWithDate: true,
 
   snackbar: {
-    horizontalPosition: 'center',
-    verticalPosition: 'top',
-    duration: 5000
-  }
+    horizontalPosition: "center",
+    verticalPosition: "top",
+    duration: 5000,
+  },
 };
